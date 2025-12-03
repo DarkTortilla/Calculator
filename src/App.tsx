bd20c05300db52d859aaeea783e88c66453c9a84
darktortilla <elgar-frank@hotmail.com>
2025-12-03 07:59:13 -0600

    initial commit; add App styles

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className='App'>
+    <div className='logo-container'>
+
+    </div>
+    <div className='calculator-container'>
+
+    </div>
+
+   </div>
+  )
+}
+
+export default App
